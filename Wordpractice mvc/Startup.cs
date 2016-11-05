4b9ad083361790566e94febefc469e1f08859b36
j_dan <j_dan@JDAN>
2016-11-05 19:52:52 +0100

    Add project files.

---
+++ Wordpractice mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wordpractice_mvc.Startup))]
+namespace Wordpractice_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
